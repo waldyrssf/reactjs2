9d7339471873a05dc0364d4374744ba13be5605f
waldyrssf <waldyr.soares11@hotmail.com>
2022-12-28 00:15:03 -0300

    atividade 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function BasicExample() {
+return (
+<Container style={{width:350, paddingTop:200}}>
+<Form> 
+<Form.Group className="mb-3" controlId="formBasicEmail">
+    <h3 class="text-center">Login</h3>
+<Form.Label>Email</Form.Label>
+<Form.Control type="email" placeholder="" />
+</Form.Group>
+<Form.Group className="mb-3" controlId="formBasicPassword">
+<Form.Label>Senha</Form.Label>
+<Form.Control type="password" placeholder="" />
+</Form.Group>
+<button class="btn btn-primary btn-block confirm-button">Acessar</button>
+</Form>
+</Container>
+);
 }
-
-export default App;
